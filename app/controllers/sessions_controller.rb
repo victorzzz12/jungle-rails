8295de541980f6165653276e3b86fb3f3388f2c2
Victor Liang <victor.zw.liang@gmail.com>
2020-07-24 02:18:02 +0000

    Reset previous commit due to database issues

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to '/'
+    else
+      # If user's login doesn't work, send them back to the login form.
+        redirect_to '/login'
+      end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
+  end
+
+end 
